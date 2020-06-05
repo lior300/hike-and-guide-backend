258747c504046dbca26e4dd6eebe2b8890e420e6
Yossi Adler <joisadler@gmail.com>
2020-06-05 21:32:42 +0300

    new build: add hamburger, fix some bugs

---
+++ public/precache-manifest.9e1735c2e75b9398e5e11e840e5c896d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544e385104804476cd862d5ed1648b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a484a3efdc58b892450",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "5466704213ce0f7ab48a",
+    "url": "/static/css/main.dabf1316.chunk.css"
+  },
+  {
+    "revision": "5a484a3efdc58b892450",
+    "url": "/static/js/2.002ef740.chunk.js"
+  },
+  {
+    "revision": "c61e735e25df9d9959071af88a4d37a2",
+    "url": "/static/js/2.002ef740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5466704213ce0f7ab48a",
+    "url": "/static/js/main.e1726d8e.chunk.js"
+  },
+  {
+    "revision": "a5167b915ef2ecbb3566",
+    "url": "/static/js/runtime-main.65244788.js"
+  },
+  {
+    "revision": "b949dc326e81712c66b152d7742e5cc7",
+    "url": "/static/media/logo.b949dc32.svg"
+  },
+  {
+    "revision": "5adbb10aac0f4a7b6c89c093e65047e0",
+    "url": "/static/media/star.5adbb10a.svg"
+  },
+  {
+    "revision": "84a0002b07c5ef1d7f180d647c8532db",
+    "url": "/static/media/‏‏star-o.84a0002b.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://192.168.0.15:3000'],
         credentials: true
     };
     app.use(cors(corsOptions));
@@ -36,6 +36,10 @@ const userRoutes = require('./api/user/user.routes')
 const trailRoutes = require('./api/trail/trail.routes')
 const orderRoutes = require('./api/order/order.routes')
 
+app.use((req, res, next) => {
+  console.log(req.url)
+  next()
+})
 
 // routes
 app.use('/api/auth', authRoutes)
@@ -45,9 +49,9 @@ app.use('/api/trail', trailRoutes)
 app.use('/api/order', orderRoutes)
 // connectSockets(io)
 
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030;
